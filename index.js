4dcda0bdba62fa3a1d258aa797fc49ca751e56bb
roganOhDev <fdscbjdcnhd@gmail.com>
2023-10-04 01:56:18 -0700

    delete solution2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { AnswerDataDto } from './dto/answerDataDto.js';
 import { OriginDataDto } from "./dto/originDataDto.js";
 
 
-function solveProblem1(originData) {
+function solveProblem(originData) {
   const originDataDtos = transformOriginDataIntoDto(originData);
 
   const mapByTimestp = createMapByTimestp(originDataDtos);
@@ -46,35 +46,4 @@ function transformOriginDataIntoDto(originData) {
 }
 
 
-//  --------------------------------------------------------------------------------------------
-
-//todo fix cnt
-function solveProblem2(originData) {
-  const groupedData = {};
-
-  originData.forEach(item => {
-    item.tracks.forEach(track => {
-      if (!groupedData[track.timestp]) {
-        groupedData[track.timestp] = {x: track.timestp, y: 0, tooltip: ''};
-      }
-      groupedData[track.timestp].y++;
-      groupedData[track.timestp].tooltip += `${track.trackName} (1), `;
-    });
-  });
-
-  for (const key in groupedData) {
-    groupedData[key].tooltip = groupedData[key].tooltip.slice(0, -2);
-  }
-
-  return Object.values(groupedData).sort((a, b) => {
-    return new Date(a.x) - new Date(b.x);
-  });
-}
-
-//  --------------------------------------------------------------------------------------------
-
-console.log(solveProblem1(sampleApiResponse));
-// console.log(solveProblem2(sampleApiResponse));
-
-
-
+console.log(solveProblem(sampleApiResponse));
